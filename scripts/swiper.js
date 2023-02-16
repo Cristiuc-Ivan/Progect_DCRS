46aa7f3163873d5336c0dfbed34dd43b73c00923
Rtiscev <alexandru.rtiscev@isa.utm.md>
2023-02-16 08:08:32 +0200

    added genres all

--- scripts/swiper.js
+++ scripts/swiper.js
@@ -2,6 +2,13 @@ const swiper = new Swiper('.swiper', {
     // Optional parameters
     direction: 'horizontal',
     loop: true,
+    effect: "fade",
+    fadeEffect: {
+        crossFade: true
+    },
+    autoplay: {
+        delay: 10000
+    },
     autoHeight: true,
     // If we need pagination
     pagination: {
