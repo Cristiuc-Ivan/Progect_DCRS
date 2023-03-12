216c370c6d1243d2d355c3b0ddfa6c6faec737af
Leomir <leomir2002@gmail.com>
2023-03-13 00:02:18 +0200

    Controllers

---
+++ Controllers/GenresController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MovieForum.Controllers
+{
+    public class GenresController : Controller
+    {
+        // GET: Home
+        public ActionResult Action()
+        {
+            return View();
+        }
+
+        public ActionResult Adventure()
+        {
+            return View();
+        }
+
+        public ActionResult Animation()
+        {
+            return View();
+        }
+        public ActionResult Comedy()
+        {
+            return View();
+        }
+        public ActionResult Crime()
+        {
+            return View();
+        }
+        public ActionResult Documentary()
+        {
+            return View();
+        }
+
+        public ActionResult Family()
+        {
+            return View();
+        }
+
+        public ActionResult Fantasy()
+        {
+            return View();
+        }
+
+        public ActionResult Horror()
+        {
+            return View();
+        }
+        public ActionResult Thriller()
+        {
+            return View();
+        }
+    }
+}
