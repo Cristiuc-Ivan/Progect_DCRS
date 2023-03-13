666f1d43813a63e85668bfdf754261a3512cc955
Cristiuc-Ivan <ivan.cristiuc@isa.utm.md>
2023-03-13 10:26:53 +0200

    fixing commit

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -3,73 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Optimization;
+using System.Web.UI;
 
-namespace MovieForum.App_Start
+namespace MovieForum
 {
-    public static class BundleConfig
+    public class BundleConfig
     {
+        // For more information on Bundling, visit https://go.microsoft.com/fwlink/?LinkID=303951
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new StyleBundle("~/bundles/bootstrap/css").Include(
-                     "~/Content/bootstrap.min.css", new CssRewriteUrlTransform()));
+            bundles.Add(new ScriptBundle("~/bundles/WebFormsJs").Include(
+                            "~/Scripts/WebForms/WebForms.js",
+                            "~/Scripts/WebForms/WebUIValidation.js",
+                            "~/Scripts/WebForms/MenuStandards.js",
+                            "~/Scripts/WebForms/Focus.js",
+                            "~/Scripts/WebForms/GridView.js",
+                            "~/Scripts/WebForms/DetailsView.js",
+                            "~/Scripts/WebForms/TreeView.js",
+                            "~/Scripts/WebForms/WebParts.js"));
 
-            bundles.Add(new StyleBundle("~/bundles/jquery/css").Include(
-                      "~/Content/jquery-ui.css", new CssRewriteUrlTransform()));
+            // Order is very important for these files to work, they have explicit dependencies
+            bundles.Add(new ScriptBundle("~/bundles/MsAjaxJs").Include(
+                    "~/Scripts/WebForms/MsAjax/MicrosoftAjax.js",
+                    "~/Scripts/WebForms/MsAjax/MicrosoftAjaxApplicationServices.js",
+                    "~/Scripts/WebForms/MsAjax/MicrosoftAjaxTimer.js",
+                    "~/Scripts/WebForms/MsAjax/MicrosoftAjaxWebForms.js"));
 
-            bundles.Add(new StyleBundle("~/bundles/genresStyles/css").Include(
-                    "~/Vendor/genresStyles.css", new CssRewriteUrlTransform()));
-
-            bundles.Add(new StyleBundle("~/bundles/login/css").Include(
-                    "~/Vendor/stylinn_login.css", new CssRewriteUrlTransform()));
-
-            bundles.Add(new StyleBundle("~/bundles/registration/css").Include(
-                   "~/Vendor/stylinn_registration.css", new CssRewriteUrlTransform()));
-
-            bundles.Add(new StyleBundle("~/bundles/mainStyle/css").Include(
-                   "~/Vendor/mainStyle.css", new CssRewriteUrlTransform()));
-
-            bundles.Add(new StyleBundle("~/bundles/stylinn/css").Include(
-                    "~/Vendor/stylinn.css", new CssRewriteUrlTransform()));
-
-            bundles.Add(new StyleBundle("~/bundles/swiper/css").Include(
-                   "~/Vendor/swiper-bundle.min.css", new CssRewriteUrlTransform()));
-
-            bundles.Add(new Bundle("~/bundles/bootstrap/js").
-                Include("~/Scripts/bootstrap.min.js"));
-
-            bundles.Add(new Bundle("~/bundles/jquery/js").
-                Include("~/Scripts/jquery-3.6.0.min.js"));
-
-            bundles.Add(new Bundle("~/bundles/addTopG/js").
-                Include("~/Vendor/Scripts/addtopG.js"));
-
-            bundles.Add(new Bundle("~/bundles/addTrending/js").
-               Include("~/Vendor/Scripts/addTrending.js"));
-
-            bundles.Add(new Bundle("~/bundles/addUpcoming/js").
-               Include("~/Vendor/Scripts/addUpcoming.js"));
-
-            bundles.Add(new Bundle("~/bundles/getForm/js").
-               Include("~/Vendor/Scripts/getForm.js"));
-
-            bundles.Add(new Bundle("~/bundles/getGenresTop/js").
-               Include("~/Vendor/Scripts/getGenresTop.js"));
-
-            bundles.Add(new Bundle("~/bundles/getTrending/js").
-               Include("~/Vendor/Scripts/getTrending.js"));
-
-            bundles.Add(new Bundle("~/bundles/getUpcoming/js").
-               Include("~/Vendor/Scripts/getUpcoming.js"));
-
-            bundles.Add(new Bundle("~/bundles/swiper/js").
-                Include("~/Vendor/Scripts/swiper.js"));
-
-            bundles.Add(new Bundle("~/bundles/swiper-bundle/js").
-                Include("~/Vendor/Scripts/swiper-bundle.min.js"));
-
-            bundles.Add(new Bundle("~/bundles/jquery-ui/js").
-                Include("~/Scripts/jquery-ui.js"));
-
+            // Use the Development version of Modernizr to develop with and learn from. Then, when you’re
+            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                            "~/Scripts/modernizr-*"));
         }
     }
 }
--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("b2d18ef3-47f3-4375-a740-8bae2c3e01b9")]
+[assembly: Guid("a21fa691-da8c-47d2-a94f-4cbc86007676")]
 
 // Version information for an assembly consists of the following four values:
 //
