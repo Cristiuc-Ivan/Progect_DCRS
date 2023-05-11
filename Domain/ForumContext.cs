2574d1c2e42ef8bc7315da29f9550fe9faca131c
Cristiuc-Ivan <ivan.cristiuc@isa.utm.md>
2023-05-11 07:55:36 +0300

    trying entities

---
+++ Domain/ForumContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class ForumContext : DbContext
+    {
+        public DbSet<UserModel> Users { get; set; }
+        public DbSet<RoleModel> Roles { get; set; }
+    }
+}
--- Domain/UserModel.cs
+++ Domain/UserModel.cs
@@ -1,20 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Domain
 {
     public class UserModel
     {
         [Key]
-        public int Id { get; set; } 
+        public int Id { get; set; }
         public string Email { get; set; }
         public string Login { get; set; }
         public string Password { get; set; }
         //public string UserRoles { get; set; }
+        public virtual ICollection<RoleModel> Roles { get; }
 
     }
+
+/*    public partial class Role
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public virtual ICollection<User_role> UserRoles { get; set; }
+    }
+
+    public partial class User_role
+    {
+        [Key, ForeignKey("User"), Column(Order = 0)]
+        public int user_id { get; set; }
+        [Key, ForeignKey("Role"), Column(Order = 1)]
+        public int role_id { get; set; }
+        public virtual Role Role { get; }
+        public virtual UserModel User { get; set; }
+    }*/
 }
