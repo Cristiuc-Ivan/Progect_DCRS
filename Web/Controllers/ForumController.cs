fe9b435a46543053eef0a8cef174cf332c92aa91
Cristiuc-Ivan <ivan.cristiuc@isa.utm.md>
2023-05-17 15:04:17 +0300

    working on forum table

---
+++ Web/Controllers/ForumController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web.Controllers
+{
+
+    public class ForumController : Controller
+    {
+        // GET: Forum
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+ //       [HttpPost]
+ //       public class ActionResult Create()
+ //       {
+ //           return View();
+ //       }
+    }
+}
